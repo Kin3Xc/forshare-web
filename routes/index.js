c492c4cd758edb2b61fb86e167545ea5eab41d7d
Kin3Xc <kin3xc@hotmail.com>
2015-09-25 01:57:46 -0500

    Modelos iniciales

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var user = require('../controllers/users');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// USERS
+router.post('/signup', user.signup);
+router.post('/login', user.login);
 
 module.exports = router;
